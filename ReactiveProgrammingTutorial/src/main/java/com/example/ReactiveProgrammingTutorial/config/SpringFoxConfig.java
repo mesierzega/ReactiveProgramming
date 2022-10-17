f8b5973e1cdde959e847fa0e22a95cba64ddcbdd
mesierzega <me.sierzega@gmail.com>
2022-10-17 09:43:26 -0400

    changed config file

--- ReactiveProgrammingTutorial/src/main/java/com/example/ReactiveProgrammingTutorial/config/SpringFoxConfig.java
+++ ReactiveProgrammingTutorial/src/main/java/com/example/ReactiveProgrammingTutorial/config/SpringFoxConfig.java
@@ -15,7 +15,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.iamiagkov.reactiveapidemo"))
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
                 .paths(PathSelectors.any())
                 .build();
     }
